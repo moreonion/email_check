e460646d0077e5a4bab76c4cd13cb299eadc0254
Roman Zimmermann <roman@more-onion.com>
2018-01-19 17:17:42 +0100

    Fix lots of coding-style issues.

--- tests/MailAlterTest.php
+++ tests/MailAlterTest.php
@@ -7,16 +7,25 @@ namespace Drupal\email_check;
  */
 class MailAlterTest extends \DrupalUnitTestCase {
 
+  /**
+   * Backup and override the $GLOBALS['conf'] variable.
+   */
   public function setUp() {
     $this->conf = $GLOBALS['conf'];
-    $GLOBALS['conf'] = [
+    $this->setConfig([
       'site_mail_domain' => 'example.com',
       'site_mail' => 'site@example.com',
       'site_replyto_mail' => 'reply-to@example.com',
-    ] + $GLOBALS['conf'];
+    ]);
   }
 
-  protected function setConfig($config) {
+  /**
+   * Helper function to override values in $GLOBALS['conf'].
+   *
+   * @param array $config
+   *   Array with config overrides.
+   */
+  protected function setConfig(array $config) {
     $GLOBALS['conf'] = $config + $GLOBALS['conf'];
   }
 
@@ -85,8 +94,7 @@ class MailAlterTest extends \DrupalUnitTestCase {
   }
 
   /**
-   * Return-Path is only set when in mail domain.
-   * Default is the site_mail.
+   * Return-Path is only set when in mail domain. Default is the site_mail.
    */
   public function testOnlyReturnPathFromMailDomain() {
     $message['from'] = 'some@example.com';
@@ -97,8 +105,7 @@ class MailAlterTest extends \DrupalUnitTestCase {
   }
 
   /**
-   * A valid return path is preserved.
-   * Default is the site_mail.
+   * A valid return path is preserved. Default is the site_mail.
    */
   public function testValidReturnPathPreserved() {
     $message['from'] = 'some@example.com';
@@ -109,8 +116,10 @@ class MailAlterTest extends \DrupalUnitTestCase {
   }
 
   /**
-   * Return-Path is not set when Return-Path and site_mail are not
-   * in site_mail_domain. Any invalid Return-Path is unset.
+   * No address in mail domain.
+   *
+   * Return-Path is not set when Return-Path and site_mail are both not in the
+   * site_mail_domain. Any invalid Return-Path is unset.
    */
   public function testNoReturnPathWhenNotMailDomain() {
     $this->setConfig(['site_mail_return_path' => 'bounce@other.com']);
@@ -134,25 +143,44 @@ class MailAlterTest extends \DrupalUnitTestCase {
     $this->assertEqual('some@example.com,another@example.com', $message['from']);
     $this->assertArrayNotHasKey('Reply-To', $message['headers']);
   }
-  // treat as primary --> replyto is set
+
+  /**
+   * Reply-To is set when first From-address is the site_mail.
+   */
   public function testMultipleHeaderFromAddressesFirstSiteMail() {
     $message['from'] = 'site@example.com, another@example.com';
     email_check_mail_alter($message);
     $this->assertEqual('site@example.com,another@example.com', $message['from']);
     $this->assertEqual('reply-to@example.com', $message['headers']['Reply-To']);
   }
+
+  /**
+   * Reply-To is not set when the first From-address is in the mail domain.
+   *
+   * The first mail address is NOT the site_mail.
+   */
   public function testMultipleHeaderFromAddressesFirstOnlyInSiteDomain() {
     $message['from'] = 'another@example.com, some@other.com';
     email_check_mail_alter($message);
     $this->assertEqual('another@example.com,some@other.com', $message['from']);
     $this->assertArrayNotHasKey('Reply-To', $message['headers']);
   }
+
+  /**
+   * Reply-To is not set when the first From-address is in the mail domain.
+   *
+   * The first mail address is NOT the site_mail.
+   */
   public function testMultipleHeaderFromAddressesFirstNotInDomainSetsReplyto() {
     $message['from'] = 'another@example.com, first@other.com, some@other.com';
     email_check_mail_alter($message);
     $this->assertEqual('another@example.com,first@other.com,some@other.com', $message['from']);
     $this->assertArrayNotHasKey('Reply-To', $message['headers']);
   }
+
+  /**
+   * Place the first address in the Reply-To if it’s not from the mail domain.
+   */
   public function testMultipleHeaderFromAddressesPrimaryAddressOnlyEverSetsReplyto() {
     $message['from'] = 'veryfirst@other.com, another@example.com, some@other.com';
     email_check_mail_alter($message);
@@ -169,6 +197,10 @@ class MailAlterTest extends \DrupalUnitTestCase {
     $this->assertEqual('site@example.com', $message['from']);
     $this->assertEqual('reply-to@example.com', $message['headers']['Reply-To']);
   }
+
+  /**
+   * Test setting Reply-To when it’s not from the site_mail_domain.
+   */
   public function testAnyReplyToWhenFromSiteMail() {
     $this->setConfig(['site_replyto_mail' => 'reply-to@other.com']);
     $message['from'] = 'site@example.com';
@@ -176,6 +208,10 @@ class MailAlterTest extends \DrupalUnitTestCase {
     $this->assertEqual('site@example.com', $message['from']);
     $this->assertEqual('reply-to@other.com', $message['headers']['Reply-To']);
   }
+
+  /**
+   * Test not setting Reply-To when From is in the site_mail_domain.
+   */
   public function testNoReplyToWhenFromSiteDomain() {
     $message['from'] = 'other@example.com';
     email_check_mail_alter($message);
@@ -191,57 +227,100 @@ class MailAlterTest extends \DrupalUnitTestCase {
     $array = _email_check_match_email_addresses($from_string);
     $this->assertEqual(array('simple@example.com'), $array);
   }
+
+  /**
+   * Test extracting multiple mailboxes.
+   */
   public function testDoubleSimpleFromAddressMatching() {
     $from_string = "simple@example.com, second@example.com";
     $array = _email_check_match_email_addresses($from_string);
     $this->assertEqual(array('simple@example.com', 'second@example.com'), $array);
   }
+
+  /**
+   * Test extracting a duplicate mailbox.
+   */
   public function testUniqueSimpleFromAddressMatching() {
     $from_string = "simple@example.com, simple@example.com  ";
     $array = _email_check_match_email_addresses($from_string);
     $this->assertEqual(array('simple@example.com'), $array);
   }
+
+  /**
+   * Test extracting multiple mailboxes with additional whitespace.
+   */
   public function testDoubleSimpleFromAddressMatchingMultipleWhitespace() {
     $from_string = "simple@example.com ,  second@example.com";
     $array = _email_check_match_email_addresses($from_string);
     $this->assertEqual(array('simple@example.com', 'second@example.com'), $array);
   }
+
+  /**
+   * Test matching a complex address.
+   */
   public function testComplexFromAddressMatching() {
     $from_string = '"This is Complex" <complex@example.com> ';
     $array = _email_check_match_email_addresses($from_string);
     $this->assertEqual(array('"This is Complex" <complex@example.com>'), $array);
   }
+
+  /**
+   * Test macthing a complex address containing commas.
+   */
   public function testComplexFromAddressMatchingWithCommaInName() {
     $from_string = '"Complex, this is" <complex@example.com> ';
     $array = _email_check_match_email_addresses($from_string);
     $this->assertEqual(array('"Complex, this is" <complex@example.com>'), $array);
   }
+
+  /**
+   * Test matching an address with commas (,) and parentheses (<>).
+   */
   public function testComplexFromAddressMatchingWithBracketsInName() {
     $from_string = '"Complex, this <aa> is" <complex@example.com> ';
     $array = _email_check_match_email_addresses($from_string);
     $this->assertEqual(array('"Complex, this <aa> is" <complex@example.com>'), $array);
   }
+
+  /**
+   * Test matching mulitple complex addresses.
+   */
   public function testDoubleComplexFromAddressMatching() {
     $from_string = '"This is Complex" <complex@example.com>, Another <another@example.com>';
     $array = _email_check_match_email_addresses($from_string);
     $this->assertEqual(array('"This is Complex" <complex@example.com>', '<another@example.com>'), $array);
   }
+
+  /**
+   * Test matching long top level domains.
+   */
   public function testSimpleFromAddressMatchingLongTld() {
     $from_string = 'simple@example.alongtld';
     $array = _email_check_match_email_addresses($from_string);
     $this->assertEqual(array('simple@example.alongtld'), $array);
   }
+
+  /**
+   * Test matching addresses with non-ascii characters.
+   */
   public function testSimpleFromAddressMatchingNotMatchingUmlautInAddress() {
     $from_string = 'hellö@example.com';
     $array = _email_check_match_email_addresses($from_string);
     $this->assertEqual(array(), $array);
   }
+
+  /**
+   * Test extracting addresses from a string with unmatched quotes.
+   */
   public function testSimpleFromAddressMatchingNotMatchingNameWhenMistakenQuote() {
     $from_string = 'A mistaken quote " in the text <simple@example.com>';
     $array = _email_check_match_email_addresses($from_string);
     $this->assertEqual(array('<simple@example.com>'), $array);
   }
 
+  /**
+   * Restore the original content of $GLOBAL['conf'].
+   */
   public function tearDown() {
     $GLOBALS['conf'] = $this->conf;
   }
